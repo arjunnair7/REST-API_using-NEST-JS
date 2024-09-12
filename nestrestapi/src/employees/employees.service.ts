3a62d0fced3077509a3b16b6ccd1682c5de56c1c
Arjun R Nair <arjunrnair323@gmail.com>
2024-09-12 11:52:04 +0530

    Employee module changes

--- nestrestapi/src/employees/employees.service.ts
+++ nestrestapi/src/employees/employees.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
+
 @Injectable()
 export class EmployeesService {
   constructor(private readonly databaseService:DatabaseService) {}
