8d4bf9681f5d7226752ac90492ab2dc6c01bf828
Arjun R Nair <arjunrnair323@gmail.com>
2024-09-18 09:25:41 +0530

    modifying buildspec

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,6 @@ async function bootstrap() {
     console.error('Database connection failed', error);
   }
 
-  // Change the port to 8080
   const port = process.env.PORT || 8080;
   await app.listen(port);
   
