37b8a71e85aaf5bbd83ce7884164f70542f07d11
Arjun R Nair <arjunrnair323@gmail.com>
2024-09-18 08:24:42 +0530

    Changing port

--- src/main.ts
+++ src/main.ts
@@ -29,8 +29,8 @@ async function bootstrap() {
     console.error('Database connection failed', error);
   }
 
-  // Use environment variable for the port or default to 3000
-  const port = process.env.PORT || 3000;
+  // Change the port to 8080
+  const port = process.env.PORT || 8080;
   await app.listen(port);
   
   console.log(`Application is running on: ${await app.getUrl()}`);
